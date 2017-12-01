17871434b04970f8e5c60b31625580bc9e3d7e5d
Marcin Rosenhof <6b4cb9@gmail.com>
2017-12-01 23:33:54 +0100

    Refactor in eucl metric.

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -10,48 +10,78 @@ class TestEucl(unittest.TestCase):
         pass
 
     def test_list_input(self):
+        """
+        Tests if function accept list arg. Should throw TypeError.
+        """
         point_a = [6, 3.1]
         point_b = [7.2, 1/3]
-        self.assertEqual(metric.eucl(point_a, point_b), np.linalg.norm(point_a - point_b))
+        self.assertRaises(TypeError, metric.eucl, point_a, point_b)
 
     def test_np_array_input(self):
+        """
+        Tests if function accept np.array as argument. It should pass.
+        """
         point_a = np.array([6, 3.1])
         point_b = np.array([7.2, 1/3])
         self.assertEqual(metric.eucl(point_a, point_b), np.linalg.norm(point_a - point_b))
 
     def test_str_input(self):
+        """
+        Tests if function accept string arg. Should throw TypeError.
+        """
         self.assertRaises(TypeError, metric.eucl, "0.312", "11")
 
     def test_te_Same_point(self):
+        """
+        Tests if distance from point to the same point is zero.
+        """
         point = np.array([6, 3.1])
         self.assertEqual(metric.eucl(point, point), 0)
         pass
 
     def test_none_arg(self):
+        """
+        Tests if function accept None arg. Should throw TypeError.
+        """
         self.assertRaises(TypeError, metric.eucl, None, 3)
 
     def test_symmetry(self):
-        point_a = [6.0000000001, 7]
-        point_b = [13.5000000001, -2]
+        """
+        Tests if distance from point A to point B is equal to distance from B to A.
+        """
+        point_a = np.array([6.0000000001, 7])
+        point_b = np.array([13.5000000001, -2])
         self.assertEqual(metric.eucl(point_a, point_b), metric.eucl(point_b, point_a))
 
     def test_1D_case(self):
+        """
+        Tests metric in 1 dimensional space. Result is compered with np.metric.
+        """
         self.assertEqual(metric.eucl(3, 6), np.linalg.norm(3-6))
 
     def test_2D_case(self):
-        point_a = [6, 7]
-        point_b = [13.5, -2]
+        """
+        Tests metric in 2 dimensional space. Result is compered with np.metric.
+        """
+        point_a = np.array([6, 7])
+        point_b = np.array([13.5, -2])
         self.assertEqual(metric.eucl(point_a, point_b), np.linalg.norm(point_a - point_b))
 
     def test_3D_case(self):
-        point_a = [6, 7, 0]
-        point_b = [13.5, -2, 3.3]
+        """
+        Tests metric in 3 dimensional space. Result is compered with np.metric.
+        """
+        point_a = np.array([6, 7, 0])
+        point_b = np.array([13.5, -2, 3.3])
         self.assertEqual(metric.eucl(point_a, point_b), np.linalg.norm(point_a - point_b))
 
     def test_nD_case(self):
+        """
+        Tests metric in multi-dimensional space. Result is compered with np.metric.
+        """
         n = 123456
-        point_a = list(range(n))
-        point_b = list(range(n, 0))
+        point_a = np.array(list(range(n)))
+        point_b = np.array(list(range(n, 0, -1)))
         self.assertEqual(metric.eucl(point_a, point_b), np.linalg.norm(point_a - point_b))
 
 

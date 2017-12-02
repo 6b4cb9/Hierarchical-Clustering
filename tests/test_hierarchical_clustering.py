a8db6ba2585c89e5074d8efb386c1de8782ecb08
Marcin Rosenhof <6b4cb9@gmail.com>
2017-12-02 03:36:59 +0100

    Hierahical clastering under test.

--- tests/test_hierarchical_clustering.py
+++ tests/test_hierarchical_clustering.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 import sklearn.cluster
 
-from hierarchical_clustering import hierarchical_clustering
+from hierarchical_clustering import hierarchical
 
 
 class TestWardClustering(unittest.TestCase):
@@ -18,13 +18,71 @@ class TestWardClustering(unittest.TestCase):
         reference = sklearn.cluster.AgglomerativeClustering(n_clusters=3, affinity="euclidean", linkage="ward")
         reference_ans = reference.fit_predict(test_data)
 
-        test = hierarchical_clustering.HierarchicalClustering(n_clusters=3, affinity="euclidean", linkage="ward")
+        test = hierarchical.HierarchicalClustering(n_clusters=3, affinity="euclidean", linkage="ward")
         test_ans = test.fit_predict(test_data)
 
         self.assertEqual(test_ans, reference_ans)
 
-    def test_l2_metric(self):
+
+class TestAverageClustering(unittest.TestCase):
+
+    def setUp(self):
         pass
+
+    def test_euclidean_in_simple_case(self):
+
+        test_data = np.array([[0, 1, 3, 12, 12, 11, 13, 1055], [-1, -1, -1, 0, 0, 0, 0, 1]])
+        test_data = test_data.transpose()
+
+        reference = sklearn.cluster.AgglomerativeClustering(n_clusters=3, affinity="euclidean", linkage="average")
+        reference_ans = reference.fit_predict(test_data)
+
+        test = hierarchical.HierarchicalClustering(n_clusters=3, affinity="euclidean", linkage="average")
+        test_ans = test.fit_predict(test_data)
+
+        self.assertEqual(test_ans, reference_ans)
+
+    def test_l2_in_simple_case(self):
+
+        test_data = np.array([[0, 1, 3, 12, 12, 11, 13, 1055], [-1, -1, -1, 0, 0, 0, 0, 1]])
+        test_data = test_data.transpose()
+
+        reference = sklearn.cluster.AgglomerativeClustering(n_clusters=3, affinity="l2", linkage="average")
+        reference_ans = reference.fit_predict(test_data)
+
+        test = hierarchical.HierarchicalClustering(n_clusters=3, affinity="l2", linkage="average")
+        test_ans = test.fit_predict(test_data)
+
+        self.assertEqual(test_ans, reference_ans)
+
+
+class TestCompleteClustering(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_euclidean_in_simple_case(self):
+        test_data = np.array([[0, 1, 3, 12, 12, 11, 13, 1055], [-1, -1, -1, 0, 0, 0, 0, 1]])
+        test_data = test_data.transpose()
+
+        reference = sklearn.cluster.AgglomerativeClustering(n_clusters=3, affinity="euclidean", linkage="complete")
+        reference_ans = reference.fit_predict(test_data)
+
+        test = hierarchical.HierarchicalClustering(n_clusters=3, affinity="euclidean", linkage="complete")
+        test_ans = test.fit_predict(test_data)
+
+        self.assertEqual(test_ans, reference_ans)
+
+    def test_l2_in_simple_case(self):
+        test_data = np.array([[0, 1, 3, 12, 12, 11, 13, 1055], [-1, -1, -1, 0, 0, 0, 0, 1]])
+        test_data = test_data.transpose()
+
+        reference = sklearn.cluster.AgglomerativeClustering(n_clusters=3, affinity="l2", linkage="complete")
+        reference_ans = reference.fit_predict(test_data)
+
+        test = hierarchical.HierarchicalClustering(n_clusters=3, affinity="l2", linkage="complete")
+        test_ans = test.fit_predict(test_data)
+
+        self.assertEqual(test_ans, reference_ans)
 
 
 if __name__ == "__main__":

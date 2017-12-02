b29bbdc7d280c41f96c9c4b7334f5b53a44fb9ed
Marcin Rosenhof <6b4cb9@gmail.com>
2017-12-02 08:29:23 +0100

    Coppy in maping.

--- tests/test_hierarchical_clustering.py
+++ tests/test_hierarchical_clustering.py
@@ -3,17 +3,31 @@ import numpy as np
 import sklearn.cluster
 import sklearn.datasets
 
+from itertools import cycle, islice
+
+import matplotlib.pyplot as plt
+
 from hierarchical_clustering import hierarchical
 
 
 def maping(n, ours, default):
+    default_copy = np.copy(default)
     remap = {k: -1 for k in range(n)}
-    size = default.size
+    size = default_copy.size
     for i in range(size):
-        if remap[default[i]] == -1:
-            remap[default[i]] = ours[i]
-        default[i] = remap[default[i]]
-    return default
+        if remap[default_copy[i]] == -1:
+            remap[default_copy[i]] = ours[i]
+        default_copy[i] = remap[default_copy[i]]
+    return default_copy
+
+
+def count(array):
+    for i in range(np.max(array) + 1):
+        ans = 0
+        for elem in array:
+            if elem == i:
+                ans += 1
+        print(str(i)+" is " + (str(ans)))
 
 
 class TestWardClustering(unittest.TestCase):
@@ -47,17 +61,32 @@ class TestWardClustering(unittest.TestCase):
         circles = sklearn.datasets.make_circles(n_samples=n_samples, factor=.5, noise=0)
         data, y = circles
 
-        reference = sklearn.cluster.AgglomerativeClustering(n_clusters=4, affinity="euclidean", linkage="average")
+        reference = sklearn.cluster.AgglomerativeClustering(n_clusters=2, affinity="euclidean", linkage="ward")
         reference_ans = reference.fit_predict(data)
 
-        test = hierarchical.HierarchicalClustering(n_clusters=4, affinity="euclidean", linkage="average")
+        test = hierarchical.HierarchicalClustering(n_clusters=2, affinity="euclidean", linkage="ward")
         test_ans = test.fit_predict(data)
-        maped_ans = maping(n=4, ours=test_ans, default=reference_ans)
+        maped_ans = reference_ans
+        print("Our:")
+        count(test_ans)
+        print("Reference:")
+        count(reference_ans)
+        maped_ans = maping(n=2, ours=test_ans, default=maped_ans)
         ans = np.array_equal(test_ans, maped_ans)
         msg = "get: " + str(test_ans) + "instead of: " + str(reference_ans) + "Maped is: " + str(maped_ans)
 
-        self.assertEqual(ans, True, msg)
 
+
+        plt.subplot(2, 1, 1)
+        colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a', '#f781bf']), int(max(test_ans) + 1))))
+        plt.scatter(data[:, 0], data[:, 1], s=2, color=colors[test_ans])
+
+        plt.subplot(2, 1, 2)
+        colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a', '#f781bf']), int(max(reference_ans) + 1))))
+        plt.scatter(data[:, 0], data[:, 1], s=2, color=colors[reference_ans])
+
+        plt.show()
+        self.assertEqual(ans, True, msg)
 
 class TestAverageClustering(unittest.TestCase):
     """

7547cd3f28a6f6f9cf7935deb8bfeafe8ed46fd6
Marcin Rosenhof <6b4cb9@gmail.com>
2017-12-02 04:06:18 +0100

    Tests of initial conditions in cluster.py.

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -2,23 +2,38 @@ import numpy as np
 import unittest
 
 from hierarchical_clustering import cluster
-from hierarchical_clustering import hierarchical_clustering
 
 
-class TestClusterInit(unittest.TestCase):
+class TestWardCluster(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_distance(self):
-        step_info = hierarchical_clustering.StepInfo()
+    def test__fail_init(self):
+        step_info = cluster.Cluster.step_info
+        self.assertRaises(Exception, step_info.select_class, "asdasd")
+
+    def test_initial_conditions(self):
+        step_info = cluster.Cluster.step_info
+        step_info.select_class("ward")
         step_info.initial_distance = np.array([[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 2], [3, 2, 1, 0]])
         step_info.current_distance = np.array([[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 2], [3, 2, 1, 0]])
         clusters = list()
         for i in range(4):
-            clusters.append(cluster.ClusterWard(i, step_info))
+            clusters.append(step_info.cluster_class(i))
         step_info.cluster_list = np.array(clusters)
-        self.assertEqual(clusters[1].distance(1, 3), 2.0)
+        self.assertEqual(step_info.cluster_class.distance(1, 3), 2.0)
 
+    def test_merge(self):
+        step_info = cluster.Cluster.step_info
+        step_info.select_class("ward")
+        step_info.initial_distance = np.array([[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 2], [3, 2, 1, 0]])
+        step_info.current_distance = np.array([[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 2], [3, 2, 1, 0]])
+        clusters = list()
+        for i in range(4):
+            clusters.append(step_info.cluster_class(i))
+        step_info.cluster_list = np.array(clusters)
+        clusters[1].merge(2)
+        self.assertEqual(str(clusters[1]), "[1 2]")
 
 
 if __name__ == '__main__':

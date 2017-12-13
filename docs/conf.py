9c438684a0be9e561519df03bfb49ddca05fbcec
Paweł Donath <pawdonapi@gmail.com>
2017-12-13 01:14:09 +0100

    Create docs

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Hierarchical-Clustering'
-copyright = u'2017, Marcin Rosenhof'
+copyright = u'2017, Paweł Donath, Marcin Rosenhof, Krystian Bytnar'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -213,7 +213,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'user_guide.tex', u'Hierarchical-Clustering Documentation',
-   u'Marcin Rosenhof', 'manual'),
+   u'Paweł Donath, Marcin Rosenhof, Krystian Bytnar', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
